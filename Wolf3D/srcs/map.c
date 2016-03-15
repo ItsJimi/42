9bd74b9d5befb58ed9bde885f45cbec9ea386091
ItsJimi <jmaiquez@student.42.fr>
2016-03-15 12:43:05 +0100

    Fix map v1

--- Wolf3D/srcs/map.c
+++ Wolf3D/srcs/map.c
@@ -6,7 +6,7 @@
 /*   By: jmaiquez <jmaiquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/10 15:38:30 by jmaiquez          #+#    #+#             */
-/*   Updated: 2016/03/14 19:16:48 by jmaiquez         ###   ########.fr       */
+/*   Updated: 2016/03/15 12:42:34 by jmaiquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,23 @@ void	map1(t_mlx *mlx, int x, int y)
 {
 	int		i;
 	int		j;
+	int		k;
 
-	i = 4;
 	j = 0;
-	while (j < i)
+	k = 10;
+	while (j < k)
 	{
-		if (mlx->p[y][x]->z == 1)
-			draw_img_map(mlx, 0xffffff, x + j, y + j);
-		else if (x == (int)mlx->beginx && y == (int)mlx->beginy)
-			draw_img_map(mlx, 0x00ff00, x + j, y + j);
-		else
-			draw_img_map(mlx, 0x000000, x + j, y + j);
+		i = 0;
+		while (i < k)
+		{
+			if (mlx->p[y][x]->z == 1)
+				draw_img_map(mlx, 0xffffff, k * x + i, k * y + j);
+			else if (x == (int)mlx->beginx && y == (int)mlx->beginy)
+				draw_img_map(mlx, 0x00ff00, k * x + i, k * y + j);
+			else
+				draw_img_map(mlx, 0x000000, k * x + i, k * y + j);
+			i++;
+		}
 		j++;
 	}
 }
