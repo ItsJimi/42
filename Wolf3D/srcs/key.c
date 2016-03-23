c47a52918947cb17709263b498ff2d2166404abb
ItsJimi <jmaiquez@student.42.fr>
2016-03-23 17:50:48 +0100

    Fix dep

--- Wolf3D/srcs/key.c
+++ Wolf3D/srcs/key.c
@@ -6,68 +6,119 @@
 /*   By: jmaiquez <jmaiquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/28 11:23:31 by jmaiquez          #+#    #+#             */
-/*   Updated: 2016/03/21 18:54:28 by jmaiquez         ###   ########.fr       */
+/*   Updated: 2016/03/23 17:49:28 by jmaiquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-static void	key_cross(int keycode, t_mlx *mlx, float a, float b)
+static void	key1(int keycode, t_mlx *mlx, float a, float b)
 {
-	if ((keycode == 123 || keycode == 12) && mlx->p[(int)(mlx->beginy + a
-	* 3)][(int)(mlx->beginx - b * 3)]->z != 1)
+	int	x;
+	int	y;
+
+	x = 1;
+	y = 1;
+	if (a < 0)
+		x = -1;
+	if (b < 0)
+		y = -1;
+	if ((keycode == 123 || keycode == 12) && mlx->p[(int)(mlx->beginy +
+	a)][(int)(mlx->beginx - b)]->z != 1 && mlx->p[(int)(mlx->beginy + a *
+	3)][(int)(mlx->beginx - b * 3)]->z != 1 && mlx->p[(int)(mlx->beginy)]
+	[(int)(mlx->beginx - b - y * 0.1)]->z != 1 && mlx->p[(int)(mlx->beginy +
+	a + x * 0.1)][(int)(mlx->beginx)]->z != 1)
 	{
 		mlx->beginx -= b;
 		mlx->beginy += a;
 	}
-	if ((keycode == 124 || keycode == 14) && mlx->p[(int)(mlx->beginy - a
-	* 3)][(int)(mlx->beginx + b * 3)]->z != 1)
+}
+
+static void	key2(int keycode, t_mlx *mlx, float a, float b)
+{
+	int	x;
+	int	y;
+
+	x = 1;
+	y = 1;
+	if (a < 0)
+		x = -1;
+	if (b < 0)
+		y = -1;
+	if ((keycode == 124 || keycode == 14) && mlx->p[(int)(mlx->beginy -
+	a)][(int)(mlx->beginx + b)]->z != 1 && mlx->p[(int)(mlx->beginy - a *
+	3)][(int)(mlx->beginx + b * 3)]->z != 1 && mlx->p[(int)(mlx->beginy)]
+	[(int)(mlx->beginx + b + y * 0.1)]->z != 1 && mlx->p[(int)(mlx->beginy +
+	a + x * 0.1)][(int)(mlx->beginx)]->z != 1)
 	{
 		mlx->beginx += b;
 		mlx->beginy -= a;
 	}
-	if ((keycode == 125 || keycode == 1) && mlx->p[(int)(mlx->beginy + b *
-	3)][(int)(mlx->beginx + a * 3)]->z != 1)
+}
+
+static void	key3(int keycode, t_mlx *mlx, float a, float b)
+{
+	int	x;
+	int	y;
+
+	x = 1;
+	y = 1;
+	if (a < 0)
+		x = -1;
+	if (b < 0)
+		y = -1;
+	if ((keycode == 125 || keycode == 1) && mlx->p[(int)(mlx->beginy +
+	b)][(int)(mlx->beginx + a)]->z != 1 && mlx->p[(int)(mlx->beginy + b *
+	3)][(int)(mlx->beginx + a * 3)]->z != 1 && mlx->p[(int)(mlx->beginy)]
+	[(int)(mlx->beginx + a + x * 0.1)]->z != 1 && mlx->p[(int)(mlx->beginy +
+	b + y * 0.1)][(int)(mlx->beginx)]->z != 1)
 	{
 		mlx->beginx += a;
 		mlx->beginy += b;
 	}
-	if ((keycode == 126 || keycode == 13) && mlx->p[(int)(mlx->beginy - b *
-	3)][(int)(mlx->beginx - a * 3)]->z != 1)
+}
+
+static void	key4(int keycode, t_mlx *mlx, float a, float b)
+{
+	int	x;
+	int	y;
+
+	x = 1;
+	y = 1;
+	if (a < 0)
+		x = -1;
+	if (b < 0)
+		y = -1;
+	if ((keycode == 126 || keycode == 13) && mlx->p[(int)(mlx->beginy -
+	b)][(int)(mlx->beginx - a)]->z != 1 && mlx->p[(int)(mlx->beginy - b *
+	3)][(int)(mlx->beginx - a * 3)]->z != 1 && mlx->p[(int)(mlx->beginy)]
+	[(int)(mlx->beginx - a - x * 0.1)]->z != 1 && mlx->p[(int)(mlx->beginy -
+	b - y * 0.1)][(int)(mlx->beginx)]->z != 1)
 	{
 		mlx->beginx -= a;
 		mlx->beginy -= b;
 	}
 }
 
-static void	key_dep(int keycode, t_mlx *mlx)
+int			key(int keycode, t_mlx *mlx)
 {
 	float	a;
 	float	b;
 
-	a = cos(mlx->deg * (PI / 180)) / 4;
-	b = sin(mlx->deg * (PI / 180)) / 4;
-	key_cross(keycode, mlx, a, b);
-	if (keycode == 0)
-		mlx->deg -= 5;
-	if (keycode == 2)
-		mlx->deg += 5;
-}
-
-static void	key_opt(int keycode, t_mlx *mlx)
-{
-	if (keycode == 53)
-		str_exit(0, "ESC !", mlx);
-}
-
-int			key(int keycode, t_mlx *mlx)
-{
 	mlx->imgview->addr = ft_memset(mlx->imgview->addr, 0, mlx->w * mlx->h * 4
 	- 1);
 	mlx->imgmap->addr = ft_memset(mlx->imgmap->addr, 0, mlx->mapw * mlx->maph *
 	4 - 1);
-	key_opt(keycode, mlx);
-	key_dep(keycode, mlx);
+	a = cos(mlx->deg * (PI / 180)) / 4;
+	b = sin(mlx->deg * (PI / 180)) / 4;
+	key1(keycode, mlx, a, b);
+	key2(keycode, mlx, a, b);
+	key3(keycode, mlx, a, b);
+	key4(keycode, mlx, a, b);
+	if (keycode == 0)
+		mlx->deg -= 5;
+	if (keycode == 2)
+		mlx->deg += 5;
 	draw(mlx);
 	return (0);
 }
