fd81f6d1144a315dbb5c186df1b177e685ac478b
ItsJimi <jmaiquez@student.42.fr>
2016-04-07 13:39:00 +0200

    Add j03 php

---
+++ Piscine-PHP/j03/ex03/cookie_crisp.php
@@ -0,0 +1,11 @@
+<?php
+	if ($_GET['action'] == "set")
+		setcookie($_GET['name'], $_GET['value']);
+	elseif ($_GET['action'] == "get")
+	{
+		if ($_COOKIE[$_GET['name']])
+			echo $_COOKIE[$_GET['name']]."\n";
+	}
+	elseif ($_GET['action'] == "del")
+		setcookie($_GET['name'], NULL, time() - 1);
+?>
---
+++ Piscine-PHP/j03/ex06/members_only.php
@@ -0,0 +1,11 @@
+<?php
+	header("WWW-Authenticate: Basic realm=''Espace membres''");
+	if ($_SERVER['PHP_AUTH_USER'] === "zaz" && $_SERVER['PHP_AUTH_PW'] === "jaimelespetitsponeys")
+	{
+		echo "<html><body>\nBonjourZaz<br />\n";
+		echo "<img src='".base64_encode(file_get_contents("../img/42.png"))."'>\n";
+		echo "</body></html>\n";
+	}
+	else
+		echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
+?>
