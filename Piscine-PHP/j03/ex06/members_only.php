0aaf38125ad6b64a7fd304ec3ac94e73f01b7c72
ItsJimi <jmaiquez@student.42.fr>
2016-04-08 22:09:38 +0200

    Fix ex06

--- Piscine-PHP/j03/ex06/members_only.php
+++ Piscine-PHP/j03/ex06/members_only.php
@@ -3,7 +3,7 @@
 	if ($_SERVER['PHP_AUTH_USER'] === "zaz" && $_SERVER['PHP_AUTH_PW'] === "jaimelespetitsponeys")
 	{
 		echo "<html><body>\nBonjourZaz<br />\n";
-		echo "<img src='".base64_encode(file_get_contents("../img/42.png"))."'>\n";
+		echo "<img src='data:image/png;base64,".base64_encode(file_get_contents("../img/42.png"))."'>\n";
 		echo "</body></html>\n";
 	}
 	else
