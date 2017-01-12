08530feea0bf97001f60f9788fe02f449075ed72
ItsJimi <jmaiquez@student.42.fr>
2017-01-12 19:11:00 +0100

    Check flags

--- ls/srcs/main.c
+++ ls/srcs/main.c
@@ -6,16 +6,17 @@
 /*   By: jmaiquez <jmaiquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/02 12:00:03 by jmaiquez          #+#    #+#             */
-/*   Updated: 2017/01/10 17:33:13 by jmaiquez         ###   ########.fr       */
+/*   Updated: 2017/01/12 19:09:30 by jmaiquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_ls.h"
+#include "../incs/ft_ls.h"
 
 int		open_dir(char *str, int sneaky)
 {
-	DIR *dir;
-	struct dirent *ent;
+	DIR				*dir;
+	struct dirent	*ent;
+
 	if ((dir = opendir(str)) != NULL)
 	{
 		while ((ent = readdir(dir)) != NULL)
@@ -36,101 +37,66 @@ int		open_dir(char *str, int sneaky)
 	}
 }
 
-char	*check_flags(int ac, char **av)
+void	if_exists(char *flags, char c, int *k)
+{
+	if (c == 'l' || c == 'R' || c == 'a' || c == 'r' || c == 't')
+	{
+		if (ft_strchr(flags, c) == NULL)
+		{
+			flags[*k] = c;
+			*k = *k + 1;
+		}
+	}
+	else
+	{
+		ft_putstr("ls: illegal option -- ");
+		ft_putchar(c);
+		ft_putstr("\nusage: ls [-lRart] [file ...]\n");
+		exit(-1);
+	}
+}
+
+void	check_flags(int ac, char **av, char *flags)
 {
 	int		i;
 	int		j;
-	char	*flags;
+	int		k;
 
 	i = 1;
-	flags = ft_strnew(5);
+	k = 0;
 	while (i < ac)
 	{
 		if (av[i][0] != '-')
-			return (flags);
-		j = 0;
+			return ;
+		j = 1;
 		while (av[i][j])
 		{
-			ft_putchar(av[i][j]);
-			flags[j] = av[i][j];
+			if_exists(flags, av[i][j], &k);
 			j++;
 		}
 		i++;
 	}
-	return (flags);
+	return ;
 }
 
 int		main(int ac, char **av)
 {
 	int		i;
-	//char	*error;
+	char	*flags;
 
 	i = 1;
+	flags = ft_strnew(5);
 	if (ac > 1)
 	{
-		ft_putstr(check_flags(ac, av));
-		// while (i < ac)
-		// {
-		// 	if (av[i][0] == '-')
-		// 	{
-		// 		if (0)
-		// 		{
-		// 			if (ft_strchr(av[i], 'l') != NULL)
-		// 			{
-		// 				ft_putstr("l");
-		// 			}
-		// 			if (ft_strchr(av[i], 'R') != NULL)
-		// 			{
-		// 				ft_putstr("R");
-		// 			}
-		// 			if (ft_strchr(av[i], 'a') != NULL)
-		// 			{
-		// 				ft_putstr("a");
-		// 			}
-		// 			if (ft_strchr(av[i], 'r') != NULL)
-		// 			{
-		// 				ft_putstr("r");
-		// 			}
-		// 			if (ft_strchr(av[i], 't') != NULL)
-		// 			{
-		// 				ft_putstr("t");
-		// 			}
-		// 		}
-		// 		else
-		// 		{
-		// 			if (!(error = (char *)malloc(sizeof(*error)
-		// 			* ft_strlen(av[1]))))
-		// 			{
-		// 				ft_putstr("malloc error");
-		// 				return (0);
-		// 		 	}
-		// 			error = ft_strsplit(av[1], '-')[1];
-		// 			ft_putstr("ls: illegal option ");
-		// 			ft_putstr(error);
-		// 		}
-		// 	}
-		// 	else
-		// 	{
-		// 		if (ac > 2)
-		// 		{
-		// 			ft_putstr(av[i]);
-		// 			ft_putstr(":\n");
-		// 		}
-		// 		open_dir(av[i], 0);
-		// 	}
-		// 	i++;
-		// 	if (i != ac)
-		// 		ft_putchar('\n');
-		// }
+		check_flags(ac, av, flags);
+		ft_putendl(flags);
+		/*
+			SOON
+		*/
 	}
 	else
 	{
 		open_dir(".", 0);
-		struct passwd *pwd;
-		struct stat statbuf;
-
-		pwd = getpwuid(statbuf.st_uid);
-		ft_putstr(statbuf.st_uid);
 	}
 	return (0);
 }
