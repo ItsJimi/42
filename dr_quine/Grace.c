bdccba0ed453396096cde6b05efdc850c34e413e
ItsJimi <jimi.maiquez@gmail.com>
2017-09-14 19:42:31 +0200

    Add err verif

--- dr_quine/Grace.c
+++ dr_quine/Grace.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define SALAD() int main() { char *a = "#include <stdio.h>%1$c#define SALAD() int main() { char *a = %2$c%3$s%2$c; FILE *f; f = fopen(%2$cGrace_kid.c%2$c, %2$cw+%2$c); fprintf(f, a, 10, 34, a); fclose(f); }%1$c#define TOMATO MIAM%1$c#define ONION MIAM%1$c/*%1$c** Pita%1$c*/%1$cSALAD()%1$c"; FILE *f; f = fopen("Grace_kid.c", "w+"); fprintf(f, a, 10, 34, a); fclose(f); }
+#define SALAD() int main() { char *a = "#include <stdio.h>%1$c#define SALAD() int main() { char *a = %2$c%3$s%2$c; FILE *f; f = fopen(%2$cGrace_kid.c%2$c, %2$cw+%2$c); if (f) { fprintf(f, a, 10, 34, a); fclose(f); }}%1$c#define TOMATO MIAM%1$c#define ONION MIAM%1$c/*%1$c** Pita%1$c*/%1$cSALAD()%1$c"; FILE *f; f = fopen("Grace_kid.c", "w+"); if (f) { fprintf(f, a, 10, 34, a); fclose(f); }}
 #define TOMATO MIAM
 #define ONION MIAM
 /*
--- dr_quine/Sully.c
+++ dr_quine/Sully.c
@@ -5,13 +5,15 @@ int main()
 	int i = 5;
 	if (i <= 0) return (0);
 	i--;
-	char *a = "#include <stdio.h>%1$c#include <stdlib.h>%1$cint main()%1$c{%1$c%4$cint i = %5$d;%1$c%4$cif (i <= 0) return (0);%1$c%4$ci--;%1$c%4$cchar *a = %2$c%3$s%2$c;%1$c%4$cFILE *f;%1$c%4$cchar str[1000];%1$c%4$csprintf(str, %2$cSully_%6$cd.c%2$c, i);%1$c%4$cf = fopen(str, %2$cw+%2$c);%1$c%4$cfprintf(f, a, 10, 34, a, 9, i, 37);%1$c%4$cfclose(f);%1$c%4$csprintf(str, %2$cgcc Sully_%6$c1$d.c -o Sully_%6$c1$d; ./Sully_%6$c1$d%2$c, i);%1$c%4$csystem(str);%1$c}%1$c";
+	char *a = "#include <stdio.h>%1$c#include <stdlib.h>%1$cint main()%1$c{%1$c%4$cint i = %5$d;%1$c%4$cif (i <= 0) return (0);%1$c%4$ci--;%1$c%4$cchar *a = %2$c%3$s%2$c;%1$c%4$cFILE *f;%1$c%4$cchar str[50];%1$c%4$csprintf(str, %2$cSully_%6$cd.c%2$c, i);%1$c%4$cf = fopen(str, %2$cw+%2$c);%1$c%4$cif (f) {%1$c%4$c%4$cfprintf(f, a, 10, 34, a, 9, i, 37);%1$c%4$c%4$cfclose(f);%1$c%4$c%4$csprintf(str, %2$cgcc Sully_%6$c1$d.c -o Sully_%6$c1$d; ./Sully_%6$c1$d%2$c, i);%1$c%4$c%4$csystem(str);%1$c%4$c}%1$c}%1$c";
 	FILE *f;
-	char str[1000];
+	char str[50];
 	sprintf(str, "Sully_%d.c", i);
 	f = fopen(str, "w+");
-	fprintf(f, a, 10, 34, a, 9, i, 37);
-	fclose(f);
-	sprintf(str, "gcc Sully_%1$d.c -o Sully_%1$d; ./Sully_%1$d", i);
-	system(str);
+	if (f) {
+		fprintf(f, a, 10, 34, a, 9, i, 37);
+		fclose(f);
+		sprintf(str, "gcc Sully_%1$d.c -o Sully_%1$d; ./Sully_%1$d", i);
+		system(str);
+	}
 }
