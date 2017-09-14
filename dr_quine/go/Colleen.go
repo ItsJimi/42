8498c286d88e87b4845bc317ee97a23b4fbf5e34
ItsJimi <jimi.maiquez@gmail.com>
2017-09-14 15:26:23 +0200

    (dr_quine) Finish golang

--- dr_quine/go/Colleen.go
+++ dr_quine/go/Colleen.go
@@ -1,17 +1,18 @@
 package main
+
 import "fmt"
 
 /*
 ** Bacon
-*/
+ */
 
 func test() int { return 0 }
 
 func main() {
 	/*
 	** Cheese
-	*/
-	var a = "package main%[1]cimport %[4]cfmt%[4]c%[1]c%[1]c/*%[1]c** Bacon%[1]c*/%[1]c%[1]cfunc test() int { return 0 }%[1]c%[1]cfunc main() {%[1]c%[2]c/*%[1]c%[2]c** Cheese%[1]c%[2]c*/%[1]c%[2]cvar a = %[4]c%[3]c%[4]c%[1]c%[2]ctest()%[1]c%[2]cfmt.Printf(a, 10, 9, a, 34)%[1]c}%[1]c%[1]c"
+	 */
+	a := "package main%[1]cimport %[4]cfmt%[4]c%[1]c%[1]c/*%[1]c** Bacon%[1]c */%[1]c%[1]cfunc test() int { return 0 }%[1]c%[1]cfunc main() {%[1]c%[2]c/*%[1]c%[2]c** Cheese%[1]c%[2]c */%[1]c%[2]ca := %[4]c%[3]c%[4]c%[1]c%[2]ctest()%[1]c%[2]cfmt.Printf(a, 10, 9, a, 34)%[1]c}%[1]c%[1]c"
 	test()
 	fmt.Printf(a, 10, 9, a, 34)
 }
