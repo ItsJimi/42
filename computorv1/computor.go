ba6fce882cc7c72080a8502679a021d8f25287e0
ItsJimi <jimi.maiquez@gmail.com>
2017-11-28 20:09:06 +0100

    (computorv1) Check validity with regex

--- computorv1/computor.go
+++ computorv1/computor.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -12,11 +13,21 @@ func main() {
 		os.Exit(1)
 	}
 	arg := strings.Replace(os.Args[1], " ", "", -1)
-	fmt.Println(arg)
+	// fmt.Println(arg)
 
-	equ := strings.Split(arg, "=")
-	if len(equ) != 2 {
+	match, _ := regexp.MatchString(`(\-?([0-9]+|[0-9]+\.[0-9]+)\*X\^0[+-]([0-9]+|[0-9]+\.[0-9]+)\*X\^1[+-]([0-9]+|[0-9]+\.[0-9]+)\*X\^2|\-?([0-9]+|[0-9]+\.[0-9]+)\*X\^0[+-]([0-9]+|[0-9]+\.[0-9]+)\*X\^1|\-?([0-9]+|[0-9]+\.[0-9]+)\*X\^0)\=(\-?([0-9]+|[0-9]+\.[0-9]+)\*X\^0[+-]([0-9]+|[0-9]+\.[0-9]+)\*X\^1[+-]([0-9]+|[0-9]+\.[0-9]+)\*X\^2|\-?([0-9]+|[0-9]+\.[0-9]+)\*X\^0[+-]([0-9]+|[0-9]+\.[0-9]+)\*X\^1|\-?([0-9]+|[0-9]+\.[0-9]+)\*X\^0)`, arg)
+	if (strings.Index(arg, "X^1") == -1 && strings.Index(arg, "X^2") == -1) || match == false {
 		fmt.Println("Wrong arguments")
 		os.Exit(1)
+	}
+
+	equ := strings.Split(arg, "=")
+	fmt.Println(equ[0])
+
+	re1, _ := regexp.Compile(`(\-?[0-9]+|\-?[0-9]+\.[0-9]+)\*X\^[0-2][+-](\-?[0-9]+|\-?[0-9]+\.[0-9]+)\*X\^[0-2][+-](\-?[0-9]+|\-?[0-9]+\.[0-9]+)\*X\^[0-2]`)
+	result := re1.FindStringSubmatch(equ[0])
+
+	for k, v := range result {
+		fmt.Printf("%d. %s\n", k, v)
 	}
 }
