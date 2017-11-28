2a4457da3b93b1d1d0025e3cf280ffbadc794795
ItsJimi <jmaiquez@student.42.fr>
2017-11-28 15:57:20 +0100

    (computorv1) Split equations

--- computorv1/computor.go
+++ computorv1/computor.go
@@ -3,13 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Wrong arguments")
-		return
+		os.Exit(1)
 	}
-	arg := os.Args[1]
+	arg := strings.Replace(os.Args[1], " ", "", -1)
 	fmt.Println(arg)
+
+	equ := strings.Split(arg, "=")
+	if len(equ) != 2 {
+		fmt.Println("Wrong arguments")
+		os.Exit(1)
+	}
 }
